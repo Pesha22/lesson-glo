3fa2e0f503dfff6173841927fa82173125f281d6
Pesha22 <pupkinn.1996@gmail.com>
2020-03-09 19:11:59 +0300

    Fixed: settings.sass create in _setting.sass

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 const {src, dest, watch} = require("gulp");
 const browserSync = require("browser-sync").create();
 const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
 
 
 // Static server
@@ -21,6 +22,9 @@ function bs() {
 function serveSass() {
     return src("./sass/**/*.sass", "./sass/**/*.scss")
         .pipe(sass())
+        .pipe(autoprefixer({
+            cascade: false
+        }))
         .pipe(dest("./css"))
         .pipe(browserSync.stream());
 };
