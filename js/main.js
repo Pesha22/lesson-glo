672edce0056107c5a098b26ed48f1100e64e1929
Pesha22 <pupkinn.1996@gmail.com>
2020-03-21 22:52:52 +0300

    решить проблему с js настроить карту

--- js/main.js
+++ js/main.js
@@ -32,54 +32,88 @@ $(document).ready(function () {
 
     new WOW().init();
 
-  // валидация формы 
-  $('.modal__form').validate({
-    errorClass: "invalid",
-    rules: {
-      // строчное правило
-      userName: {
-        reguired: true,
-        minlength: 2
-      },
-      userPhone: "reguired",
-      //правило объеут (блок)
-      userEmail: {
-        reguired: true,
-        email: true
-      }
-    }, // сообщения 
-    messages: {
-      userName: {
-        required: "Имя обязательно",
-        minlength: "Имя не короче двух букв"
-      },
-      userPhone: "Телефон обязателен",
-      userEmail: {
-        reguired:"Обязательно укажите email",
-        email:"Введите в формате: name@domain.com"
-      }
+// валидация формы 
+$('.modal__form').validate({
+  errorClass: "invalid",
+  rules: {
+    // строчное правило
+    userName: {
+      reguired: true,
+      minlength: 2,
+    },
+    userPhone: "reguired",
+    //правило объеут (блок)
+    userEmail: {
+      reguired: true,
+      email: true,
     }
-  });
+  }, // сообщения 
+  messages: {
+    userName: {
+      required: "Имя обязательно",
+      minlength: "Имя не короче двух букв",
+    },
+    userPhone: "Телефон обязателен",
+    userEmail: {
+      reguired:"Обязательно укажите email",
+      email:"Введите в формате: name@domain.com",
+    },
+  },
+});
   
   // маска для телефона
   $('[type=tel]').mask('+7(000) 000-00-00', {placeholder: "+7 (___) ___ __-__"});
 
   // создание яндекс карты 
-  // Функция ymaps.ready() будет вызвана, когда
-  // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
-  ymaps.ready(init);
-  function init(){
-      // Создание карты.
-      var myMap = new ymaps.Map("map", {
-          // Координаты центра карты.
-          // Порядок по умолчанию: «широта, долгота».
-          // Чтобы не определять координаты центра карты вручную,
-          // воспользуйтесь инструментом Определение координат.
-          center: [55.76, 37.64],
-          // Уровень масштабирования. Допустимые значения:
-          // от 0 (весь мир) до 19.
-          zoom: 7
-      });
-  }
+  newFunction();
 
 });
+function newFunction() {
+  ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+      center: [55.751574, 37.573856],
+      zoom: 9
+    }, {
+      searchControlProvider: 'yandex#search'
+    }),
+      // Создаём макет содержимого.
+      MyIconContentLayout = ymaps.templateLayoutFactory.createClass('<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'), myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+        hintContent: 'Собственный значок метки',
+        balloonContent: 'Это красивая метка'
+      }, {
+        // Опции.
+        // Необходимо указать данный тип макета.
+        iconLayout: 'default#image',
+        // Своё изображение иконки метки.
+        iconImageHref: 'images/myIcon.gif',
+        // Размеры метки.
+        iconImageSize: [30, 42],
+        // Смещение левого верхнего угла иконки относительно
+        // её "ножки" (точки привязки).
+        iconImageOffset: [-5, -38]
+      }), myPlacemarkWithContent = new ymaps.Placemark([55.661574, 37.573856], {
+        hintContent: 'Собственный значок метки с контентом',
+        balloonContent: 'А эта — новогодняя',
+        iconContent: '12'
+      }, {
+        // Опции.
+        // Необходимо указать данный тип макета.
+        iconLayout: 'default#imageWithContent',
+        // Своё изображение иконки метки.
+        iconImageHref: 'images/ball.png',
+        // Размеры метки.
+        iconImageSize: [48, 48],
+        // Смещение левого верхнего угла иконки относительно
+        // её "ножки" (точки привязки).
+        iconImageOffset: [-24, -24],
+        // Смещение слоя с содержимым относительно слоя с картинкой.
+        iconContentOffset: [15, 15],
+        // Макет содержимого.
+        iconContentLayout: MyIconContentLayout
+      });
+    myMap.geoObjects
+      .add(myPlacemark)
+      .add(myPlacemarkWithContent);
+  });
+}
+
